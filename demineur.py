cfd270ddb8bb4787ba4265a50590ba790f24fe8d
camara_aicha <aicha.ca@outlook.fr>
2024-03-22 22:28:20 +0100

    ajouts de tout les commentaires

--- demineur.py
+++ demineur.py
@@ -9,19 +9,23 @@ class Demineur:
         self.mines_placees = False
 
     def placer_mines(self, premier_clic):
+        # Vérifie si les mines n'ont pas déjà été placées
         if not self.mines_placees:
-            x, y = premier_clic
-            indices = [(i, j) for i in range(self.hauteur) for j in range(self.largeur)]
-            indices.remove((x, y))
-            mines_a_placer = random.sample(indices, self.mines)
+            x, y = premier_clic  # Récupère les coordonnées du premier clic
+            indices = [(i, j) for i in range(self.hauteur) for j in
+                       range(self.largeur)]  # Génère tous les indices possibles dans la grille
+            indices.remove((x, y))  # Supprime les coordonnées du premier clic de la liste des indices
+            mines_a_placer = random.sample(indices,
+                                           self.mines)  # Sélectionne aléatoirement les emplacements pour les mines
             for x, y in mines_a_placer:
-                self.grille[x][y] = -1
-            self.mines_placees = True
+                self.grille[x][y] = -1  # Place les mines dans la grille
+            self.mines_placees = True  # Indique que les mines ont été placées
 
     def compter_bombes_adjacentes(self, row, col):
-        compteur = 0
+        compteur = 0  # Initialise le compteur de bombes adjacentes
+        # Parcourt les cases adjacentes à la case donnée
         for i in range(max(0, row - 1), min(self.hauteur, row + 2)):
             for j in range(max(0, col - 1), min(self.largeur, col + 2)):
-                if self.grille[i][j] == -1:
-                    compteur += 1
-        return compteur
+                if self.grille[i][j] == -1:  # Vérifie si la case contient une bombe
+                    compteur += 1  # Incrémente le compteur si une bombe est trouvée
+        return compteur  # Retourne le nombre de bombes adjacentes à la case donnée
